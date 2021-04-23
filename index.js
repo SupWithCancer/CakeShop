5ae79eac34ae7aaae11d74c1e6c7b38279d0f8a3
SupWithCancer <fanni2661@gmail.com>
2021-04-23 16:33:59 +0300

    commit message

---
+++ index.js
@@ -0,0 +1,5 @@
+﻿let menuButton = document.getElementById("burger-icon");
+const headerMobile = document.getElementById("links-mobile");
+menuButton.addEventListener("click", function () {
+  headerMobile.classList.toggle("hidden");
+});
